6b6d42f422b256dbc25408e088597aa940fd1b9e
Ly William Chhim <lchhim@octo.com>
2017-06-26 10:22:30 +0200

    [FC-901] Display retrieved values in agent mode

--- serviceProviderNodeJSPassport/controllers/index.js
+++ serviceProviderNodeJSPassport/controllers/index.js
@@ -1,19 +1,21 @@
 'use strict;'
 
 var IndexController = function(){};
+var configManager = new (require('../helpers/configManager.js'))();
 
 IndexController.prototype.handleMain = function(req, res){
+    var agentMode = configManager.isModeAgents();
     if(req.session.user){
-        res.render('index', {title: 'Démonstrateur France Connect - Accueil', user: req.session.user, userInfo: req.session.userInfo});
+        res.render('index', {title: 'Démonstrateur France Connect - Accueil', user: req.session.user, userInfo: req.session.userInfo, agentMode: agentMode});
     } else {
         if(req.session.passport && req.session.passport.user){
             var given_name = (req.session.passport.user._json.given_name) ? req.session.passport.user._json.given_name : '';
             var family_name = (req.session.passport.user._json.family_name) ? req.session.passport.user._json.family_name : '';
             req.session.user = given_name + ' ' + family_name;
             var userInfo = (req.session.passport.user._json) ? req.session.passport.user._json : '';
-            res.render('index', {title: 'Démonstrateur France Connect - Accueil', user: req.session.user, userInfo: userInfo});
+            res.render('index', {title: 'Démonstrateur France Connect - Accueil', user: req.session.user, userInfo: userInfo, agentMode: agentMode});
         } else {
-            res.render('index', {title: 'Démonstrateur France Connect - Accueil', user: undefined, userInfo: undefined});
+            res.render('index', {title: 'Démonstrateur France Connect - Accueil', user: undefined, userInfo: undefined, agentMode: agentMode});
         }
     }
 };
