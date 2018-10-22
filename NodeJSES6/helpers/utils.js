cc3ec91d4017de004148f41f96f600f311e67750
SandyCT97 <sandy.pierre@consertotech.pro>
2018-10-22 13:11:05 +0200

    Add config manager and modify config params

--- NodeJSES6/helpers/utils.js
+++ NodeJSES6/helpers/utils.js
@@ -4,15 +4,16 @@
  * @see @link{ https://partenaires.franceconnect.gouv.fr/fcp/fournisseur-service# }
  */
 import config from './configManager';
-// TODO hard code state et nonce because they normaly generate from every request
-export const getAuthorizationUrl = () => `${config.AUTHORIZATION_URL}?response_type=code`
-    + `&client_id=${config.CLIENT_SECRET}&redirect_uri=${config.REDIRECT_URL}`
-    + `&scope=${config.SCOPE}&state=customState11&nonce=customNonce11`;
+// TODO hard code state et nonce because they normally generate from every request
+export const getAuthorizationUrl = () => `${config.FC_URL}${config.AUTHORIZATION_FC_PATH}?`
+  + `response_type=code&client_id=${config.CLIENT_ID}&redirect_uri=${config.FS_URL}`
+  + `${config.CALLBACK_FS_PATH}&scope=${config.SCOPES}&state=customState11&nonce=customNonce11`;
 
 
 /**
  * Format the url 's that is used in a redirect call to France Connect logout API endpoint
  * @returns {string}
  */
-export const getLogoutUrl = req => `${config.LOGOUT_URL}?id_token_hint=${req.session.idToken}`
-  + `&state=customState11&post_logout_redirect_uri=${config.LOGOUT_REDIRECT_URL}`;
+export const getLogoutUrl = req => `${config.FC_URL}${config.LOGOUT_FC_PATH}?id_token_hint=`
+  + `${req.session.idToken}&state=customState11&post_logout_redirect_uri=${config.FS_URL}`
+  + `${config.LOGOUT_FS_PATH}`;
