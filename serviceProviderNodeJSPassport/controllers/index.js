c4d5eef8744b70bafb48f23f9583222f00ab52d9
Emmanuel Maravilha <emaravilha@kaliopmail.com>
2019-02-26 17:16:33 +0100

    [FCA-115] FC/FCA replaced by AC

--- serviceProviderNodeJSPassport/controllers/index.js
+++ serviceProviderNodeJSPassport/controllers/index.js
@@ -6,16 +6,16 @@ const configManager = new (require('../helpers/configManager.js'))();
 IndexController.prototype.handleMain = function(req, res){
     const agentMode = configManager.isModeAgents();
     if(req.session.user){
-        res.render('index', {title: 'Démonstrateur France Connect - Accueil', user: req.session.user, userInfo: req.session.userinfos, agentMode: agentMode});
+        res.render('index', {title: 'Démonstrateur AgentConnect - Accueil', user: req.session.user, userInfo: req.session.userinfos, agentMode: agentMode});
     } else {
         if(req.session.passport && req.session.user){
             const given_name = (req.session.userinfos.given_name) ? req.session.userinfos.given_name : '';
             const family_name = (req.session.userinfos.family_name) ? req.session.userinfos.family_name : '';
             req.session.user = given_name + ' ' + family_name;
             const userInfo = (req.session.userinfos) ? req.session.userinfos : '';
-            res.render('index', {title: 'Démonstrateur France Connect - Accueil', user: req.session.user, userInfo: userInfo, agentMode: agentMode});
+            res.render('index', {title: 'Démonstrateur AgentConnect - Accueil', user: req.session.user, userInfo: userInfo, agentMode: agentMode});
         } else {
-            res.render('index', {title: 'Démonstrateur France Connect - Accueil', user: undefined, userInfo: undefined, agentMode: agentMode});
+            res.render('index', {title: 'Démonstrateur AgentConnect - Accueil', user: undefined, userInfo: undefined, agentMode: agentMode});
         }
     }
 };
